e8da80897ad3dbdcba3624d22b59b86631460e81
DESKTOP-CKK2L6K\Эльдар <elfarych@gmail.com>
2022-08-05 03:35:15 +0600

    +

--- dogsswap-main-app/dogsswap-main-app-front/src/components/wallet/contracts/approve-quantity-setter.js
+++ dogsswap-main-app/dogsswap-main-app-front/src/components/wallet/contracts/approve-quantity-setter.js
@@ -1,5 +1,3 @@
 export default function approveQuantitySetter () {
-  const val = 2 ** 256 - 1
-  console.log(val)
-  return val
+  return '115792089237316195423570985008687907853269984665640564039457584007913129639935'
 }
